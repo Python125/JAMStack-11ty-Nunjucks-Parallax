5c8b4faac4bf983d632a5d753892f58e518d4639
PythonMonty <andrewnovosel90@gmail.com>
2024-10-21 12:08:35 -0400

    Fixed margin-left for .blog-text2

---
+++ src/script.js
@@ -0,0 +1,68 @@
+window.addEventListener("load", () => {
+  const homeImage = document.querySelector(".home-image");
+
+  homeImage.style.opacity = "0";
+  homeImage.style.transform = "translateY(-100%)";
+
+  setTimeout(() => {
+    homeImage.style.opacity = "1";
+    homeImage.style.transform = "translateY(0)";
+  }, 500);
+});
+
+window.addEventListener("load", () => {
+  const homeText = document.querySelector(".home-text");
+
+  homeText.style.opacity = "0";
+  homeText.style.transform = "translateY(100%)";
+
+  setTimeout(() => {
+    homeText.style.opacity = "1";
+    homeText.style.transform = "translateX(0)";
+  }, 900);
+});
+
+window.addEventListener("load", () => {
+  const homeButton = document.querySelector(".home-btn");
+
+  homeButton.style.opacity = "0";
+  homeButton.style.transform = "translateX(-100%)";
+
+  setTimeout(() => {
+    homeButton.style.opacity = "1";
+    homeButton.style.transform = "translateX(0)";
+  }, 1400);
+});
+
+window.addEventListener("load", () => {
+  const blogImage = document.querySelector(".blog-image");
+
+  setTimeout(() => {
+    blogImage.style.opacity = "1";
+    blogImage.style.transform = "translateX(0)";
+  }, 250);
+});
+
+window.addEventListener("load", () => {
+  const blogText1 = document.querySelector(".blog-text1");
+
+  blogText1.style.opacity = "0";
+  blogText1.style.transform = "translateY(10000vh)";
+
+  setTimeout(() => {
+    blogText1.style.opacity = "1";
+    blogText1.style.transform = "translateY(0)";
+  }, 155);
+});
+
+window.addEventListener("load", () => {
+  const blogText2 = document.querySelector(".blog-text2");
+
+  blogText2.style.opacity = "0";
+  blogText2.style.transform = "translateY(10000vh)";
+
+  setTimeout(() => {
+    blogText2.style.opacity = "1";
+    blogText2.style.transform = "translateY(0)";
+  }, 250);
+});
